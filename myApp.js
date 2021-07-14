1e5371db6f7223f674ede88e0bd19ef61d40150c
kimSpiegel04 <k.spiegel72@gmail.com>
2021-07-14 11:10:31 -0400

    serve files and send some json

--- myApp.js
+++ myApp.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
 
+const absolutePath = __dirname + '/views/index.html';
+const stylePath = __dirname + '/public';
 
+
+app.get('/', (req, res) => {
+  // res.send('Hello Express');
+    res.sendFile(absolutePath);
+});
+
+app.use('/public', express.static(stylePath));
+
+app.get('/json', (req,res) => {
+    res.json({"message": "Hello json"});
+})
 
 
 
